5d75474110b7706ff89a8d5925f8d38c24cdadd6
Jonathan Arevalo <jonathan.arevalo92@gmail.com>
2013-04-18 22:02:58 +0200

    Minor changes in CCindex.php

--- src/CCIndex/CCIndex.php
+++ src/CCIndex/CCIndex.php
@@ -2,7 +2,7 @@
 /**
  * Standard controller layout.
  * 
- * @package LydiaCore
+ * @package LuminaCore
  */
 class CCIndex implements IController {
 
@@ -10,9 +10,9 @@ class CCIndex implements IController {
     * Implementing interface IController. All controllers must have an index action.
    */
   public function Index() {  
-    global $ly;
-    $ly->data['title'] = "The Index Controller";
-    $ly->data['main'] = "<h1>The Index Controller</h1>";
+    global $lu;
+    $lu->data['title'] = "The Index Controller";
+    $lu->data['main'] = "<h1>The Index Controller</h1>";
   }
 
 }  
